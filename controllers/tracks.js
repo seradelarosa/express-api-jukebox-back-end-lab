f5b526f401d407d5b23b747af33e8dee4b694e9d
Sera De La Rosa <sera.delarosa@outlook.com>
2025-03-11 15:24:54 -0400

    Get route done

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -17,11 +17,14 @@ router.post('/', async (req, res) => {
 
 // READ - GET - /tracks
 
-
-
-
-
-
+router.get('/', async (req, res) => {
+    try {
+        const foundTracks = await Track.find();
+        res.status(200).json(foundTracks);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
 
 
 
