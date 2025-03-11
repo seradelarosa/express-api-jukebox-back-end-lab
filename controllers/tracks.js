434dd6988cb396f97b1a6d11735022ea6fb5d4a8
Sera De La Rosa <sera.delarosa@outlook.com>
2025-03-11 15:23:55 -0400

    Create route done

---
+++ controllers/tracks.js
@@ -0,0 +1,39 @@
+const Track = require('../models/track.js');
+const express = require('express');
+const router = express.Router();
+
+//== CRUD routes ==============================================
+
+//CREATE - POST - /tracks
+router.post('/', async (req, res) => {
+    try {
+        const createdTrack = await Track.create(req.body);
+
+        res.status(201).json(createdTrack);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    };
+});
+
+// READ - GET - /tracks
+
+
+
+
+
+
+
+
+
+// SHOW SPECIFIC TRACK - READ - GET - /tracks/:id
+
+// DELETE - /tracks/:id
+
+// UPDATE - PUT - /tracks/:id
+
+
+
+
+//=============================================================
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,16 +5,31 @@ const app = express();
 const mongoose = require('mongoose');
 const logger = require('morgan');
 
+const tracksRouter = require('./controllers/tracks.js');
+
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+//== middleware ============================================
+
 app.use(express.json());
 app.use(logger('dev'));
 
+app.use('/tracks', tracksRouter);
+
+//==========================================================
+
 // Routes go here
+
+
+
+
+
+
+//==========================================================
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
